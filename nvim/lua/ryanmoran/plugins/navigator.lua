db342931ecfed3947fc1545a2f7d861ea8a9ebc4
Ryan Moran <ryan.moran@gmail.com>
2024-06-07 08:59:37 -0700

    Adds goimports on file save

--- nvim/lua/ryanmoran/plugins/navigator.lua
+++ nvim/lua/ryanmoran/plugins/navigator.lua
@@ -36,5 +36,16 @@ return {
             end,
             group = vim.api.nvim_create_augroup("go_autocommands", {clear = true})
         })
+
+        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+               require('go.format').goimports()
+            end,
+            group = format_sync_grp,
+        })
+
     end
 }
+
