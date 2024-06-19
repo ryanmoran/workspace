91f16436e3d7c2ba6c96ca7750533dfb51eb55df
Ryan Moran <ryan.moran@gmail.com>
2024-06-19 10:26:41 -0700

    Brings back navigator

--- nvim/lua/ryanmoran/plugins/navigator.lua
+++ nvim/lua/ryanmoran/plugins/navigator.lua
@@ -36,16 +36,6 @@ return {
             end,
             group = vim.api.nvim_create_augroup("go_autocommands", {clear = true})
         })
-
-        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
-        vim.api.nvim_create_autocmd("BufWritePre", {
-            pattern = "*.go",
-            callback = function()
-               require('go.format').goimports()
-            end,
-            group = format_sync_grp,
-        })
-
     end
 }
 
