619a239177c24bfddd73ff84b39c3b137df17b37
Ryan Moran <ryan.moran@gmail.com>
2024-06-19 11:02:25 -0700

    Disable double formatting on save

--- nvim/lua/ryanmoran/plugins/navigator.lua
+++ nvim/lua/ryanmoran/plugins/navigator.lua
@@ -17,24 +17,16 @@ return {
         require("go").setup()
         require("navigator").setup({
             lsp_signature_help = true, -- enable ray-x/lsp_signature
-            lsp = {format_on_save = true}
+            lsp = {format_on_save = {disable = {'go'}}}
         })
 
-        vim.api.nvim_create_autocmd("FileType", {
-            pattern = {"go"},
-            callback = function(ev)
-                -- CTRL/control keymaps
-                vim.api .nvim_buf_set_keymap(0, "n", "<C-i>", ":GoImport<CR>", {})
-                vim.api.nvim_buf_set_keymap(0, "n", "<C-b>", ":GoBuild %:h<CR>", {})
-                vim.api.nvim_buf_set_keymap(0, "n", "<C-t>", ":GoTestPkg<CR>", {})
-                vim.api.nvim_buf_set_keymap(0, "n", "<C-c>", ":GoCoverage -p<CR>", {})
-
-                -- Opens test files
-                vim.api.nvim_buf_set_keymap(0, "n", "tt", ":lua require('go.alternate').switch(true, '')<CR>", {}) -- Test
-                vim.api.nvim_buf_set_keymap(0, "n", "tv", ":lua require('go.alternate').switch(true, 'vsplit')<CR>", {}) -- Test Vertical
-                vim.api.nvim_buf_set_keymap(0, "n", "ts", ":lua require('go.alternate').switch(true, 'split')<CR>", {}) -- Test Split
+        local format_sync_grp = vim.api.nvim_create_augroup("GoFormat", {})
+        vim.api.nvim_create_autocmd("BufWritePre", {
+            pattern = "*.go",
+            callback = function()
+               require('go.format').goimports()
             end,
-            group = vim.api.nvim_create_augroup("go_autocommands", {clear = true})
+            group = format_sync_grp,
         })
     end
 }
